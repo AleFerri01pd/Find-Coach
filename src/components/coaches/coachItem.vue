82a7c714b4f10416c3fa01336060394c6d9e6593
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-06-23 11:54:26 +0200

    Sezione coach List

---
+++ src/components/coaches/coachItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <li>
+        <h3> {{ fullName }} </h3>
+        <h4> ${{ rate }}/hour </h4>
+        <div>
+            <span v-for="area in areas" :key="area">{{ area }}</span>
+        </div>
+        <div class="actions">
+            <router-link :to="coachContatcLink">Contatti</router-link>
+            <router-link :to="coachDetailsLink">Guarda dettagli</router-link>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+    computed: {
+        fullName() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        coachContatcLink() {
+            return this.$route.path + '/' + this.id + '/contact';
+        },
+        coachDetailsLink() {
+            return this.$route.path + '/' + this.id + '/contact';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+@import '../../../public/css/coach-item.css';
+
+</style>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,16 +8,25 @@
             <router-link to="/register">Registra Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-            {{ coach.firstName }}
-            </li>
+            <coach-item v-for="coach in filteredCoaches" 
+            :key="coach.id" 
+            :id="coach.id"
+            :firstName="coach.firstName"
+            :lastName="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"></coach-item>
         </ul>
         <h3 v-else>Nessun Allenatore Trovato...</h3>
     </section>
 </template>
 
 <script>
+import coachItem from '../../components/coaches/coachItem.vue';
+
 export default {
+    components: {
+        coachItem
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
@@ -28,3 +37,8 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+@import '../../../public/css/coaches-list.css';
+
+</style>
