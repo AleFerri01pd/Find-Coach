5fd078b792b41498945cf2add95401af30c29898
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-06-23 17:17:00 +0200

    Base UI Component

--- src/components/coaches/coachItem.vue
+++ src/components/coaches/coachItem.vue
@@ -3,11 +3,11 @@
         <h3> {{ fullName }} </h3>
         <h4> ${{ rate }}/hour </h4>
         <div>
-            <span v-for="area in areas" :key="area">{{ area }}</span>
+            <base-badge v-for="area in areas" :key="area" :type="area" :title="area">{{ area }}</base-badge>
         </div>
         <div class="actions">
-            <router-link :to="coachContatcLink">Contatti</router-link>
-            <router-link :to="coachDetailsLink">Guarda dettagli</router-link>
+            <base-button link :to="coachContatcLink" mode="outline">Contatti</base-button>
+            <base-button link :to="coachDetailsLink">Guarda dettagli</base-button>
         </div>
     </li>
 </template>
@@ -31,6 +31,29 @@ export default {
 
 <style scoped>
 
-@import '../../../public/css/coach-item.css';
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
 
 </style>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -2,21 +2,24 @@
     <section name="filter">
         FILTER
     </section>
+    
     <section name="list">
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Registra Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item v-for="coach in filteredCoaches" 
-            :key="coach.id" 
-            :id="coach.id"
-            :firstName="coach.firstName"
-            :lastName="coach.lastName"
-            :rate="coach.hourlyRate"
-            :areas="coach.areas"></coach-item>
-        </ul>
-        <h3 v-else>Nessun Allenatore Trovato...</h3>
+        <base-card>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button>
+                <base-button link to="/register">Registra Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" 
+                :key="coach.id" 
+                :id="coach.id"
+                :firstName="coach.firstName"
+                :lastName="coach.lastName"
+                :rate="coach.hourlyRate"
+                :areas="coach.areas"></coach-item>
+            </ul>
+            <h3 v-else>Nessun Allenatore Trovato...</h3>
+        </base-card>
     </section>
 </template>
 
@@ -25,8 +28,8 @@ import coachItem from '../../components/coaches/coachItem.vue';
 
 export default {
     components: {
-        coachItem
-    },
+        coachItem,
+},
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
