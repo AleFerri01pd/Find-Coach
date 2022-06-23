ab69da7db8d50ffe3cfb0930fd9ece21faad1a83
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-06-23 11:37:21 +0200

    Aggiunto store (vuex) e bind con componenti

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,9 +7,24 @@
             <button>Refresh</button>
             <router-link to="/register">Registra Coach</router-link>
         </div>
+        <ul v-if="hasCoaches">
+            <li v-for="coach in filteredCoaches" :key="coach.id">
+            {{ coach.firstName }}
+            </li>
+        </ul>
+        <h3 v-else>Nessun Allenatore Trovato...</h3>
     </section>
 </template>
 
 <script>
-
+export default {
+    computed: {
+        filteredCoaches() {
+            return this.$store.getters['coaches/coaches'];
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches'];
+        }
+    }
+}
 </script>
