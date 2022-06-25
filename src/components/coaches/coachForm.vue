915f4abeba1cd79feebd7f56d68af44f328b0411
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-06-25 14:56:44 +0200

    Componente per Registrazione nuovo istruttore

---
+++ src/components/coaches/coachForm.vue
@@ -0,0 +1,120 @@
+<template>
+    <form @submit.prevent="submitForm">
+        <div class="form-control">
+            <label>Nome</label>
+            <input type="text" id="firstname" v-model.trim="firstName"/>
+        </div>
+        <div class="form-control">
+            <label>Cognome</label>
+            <input type="text" id="lastname" v-model.trim="lastName"/>
+        </div>
+        <div class="form-control">
+            <label>Descrizione</label>
+            <textarea id="lastname" rows="5" v-model.trim="description"></textarea>
+        </div>
+        <div class="form-control">
+            <label>Guadagno per ora</label>
+            <input type="number" id="rate" v-model.number="rate"/>
+        </div>
+        <div class="form-control">
+            <h3>Aree di interesse</h3>
+            <div>
+                <input type="checkbox" id="frontend" value="frontend" v-model="areas"/>
+                <label for="frontend">Sviluppo Frontend</label>
+            </div>
+            <div>
+                <input type="checkbox" id="backend" value="backend" v-model="areas"/>
+                <label for="backend">Sviluppo Backend</label>
+            </div>
+            <div>
+                <input type="checkbox" id="career" value="career" v-model="areas"/>
+                <label for="career">Career</label>
+            </div>
+        </div>
+        <base-button>Registra</base-button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            firstName: '',
+            lastName: '',
+            description: '',
+            rate: null,
+            areas: [],
+        }
+    },
+    methods: {
+        submitForm() {
+            const formData = {
+                firt: this.firstName,
+                last: this.lastName,
+                rate: this.rate,
+                description: this.description,
+                areas: this.areas
+            }
+
+            console.log(formData);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type='checkbox'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type='checkbox']:focus {
+  outline: #3d008d solid 1px;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+</style>
--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,18 @@
 <template>
-    Register
+    <section name="registration">
+        <base-card>
+            <h2>Registra un nuovo Istruttore ora !!</h2>
+            <form-coach></form-coach>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import FormCoach from '../../components/coaches/coachForm.vue';
+
+export default {
+    components: {
+        FormCoach,
+    }
+}
+</script>
