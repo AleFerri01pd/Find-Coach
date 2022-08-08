5b7234cadc2527ed6d48b1227ff67ee7c6e03467
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-08-08 17:34:04 +0200

    Implementazione getters per requests

--- client/src/pages/requests/ContactCoach.vue
+++ client/src/pages/requests/ContactCoach.vue
@@ -36,8 +36,10 @@ export default {
             this.$store.dispatch('requests/contactCoach', {
               email: this.email,
               message: this.message,
-              coachId: this.$route.params.id
+              id: this.$route.params.id
             });
+
+console.log(this.$route.params.id);
 
             this.$router.replace('/coaches');
         }
